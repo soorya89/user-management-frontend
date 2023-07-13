3ca79ed79b9b4090cef43f78a52d8fb69f2621a3
jishnusanthosh <jishnusanthosh321@gmail.com>
2023-07-13 22:16:12 +0530

    first=commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Homepage from './Components/homepage/Homepage.js';
+import Login from './Components/login/Login.js';
+import Register from './Components/register/Register.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Homepage></Homepage>
+      <Login></Login>
+      <Register></Register>
+  
     </div>
   );
 }
